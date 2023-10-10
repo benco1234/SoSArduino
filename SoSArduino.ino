52c49b60f819f06c6ecbe63d4a568fb2a6b5fa65
benco <benco@192.168.20.255>
2023-10-10 14:56:43 -0400

    I made the router move
    I made an SOS function because Mr. Harrington told me to

--- SoSArduino.ino
+++ SoSArduino.ino
@@ -1,17 +1,36 @@
 int pinnumber=13; 
 
+#include <Servo.h>
+
+Servo myServo; 
+
 void setup() {
   // put your setup code here, to run once:
 pinMode(LED_BUILTIN,OUTPUT);
+  myServo.attach(11);
+  myServo.write(90);
 }
 
 void blinkOnce(int time){
   digitalWrite(LED_BUILTIN, HIGH);
+  myServo.write(140);
   delay(time);
   digitalWrite(LED_BUILTIN, LOW);
+  myServo.write(60);
   delay(time);
 }
 
-void loop() {
+void BlinkSOS() {
   blinkOnce(1000);
+  blinkOnce(1000);
+  blinkOnce(1000);
+  blinkOnce(900);
+  blinkOnce(900);
+  blinkOnce(900);
+  blinkOnce(1000);
+  blinkOnce(1000);
+  blinkOnce(1000);
+}
+void loop(){
+  BlinkSOS();
 }
