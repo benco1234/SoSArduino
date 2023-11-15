ff0dfaf17f7e7aa9df2ca184c77e697207c1446d
benco <benco@192.168.20.255>
2023-11-15 10:07:14 -0500

    upload

--- SoSArduino.ino
+++ SoSArduino.ino
@@ -1,36 +1,40 @@
-int pinnumber=13; 
-
 #include <Servo.h>
 
 Servo myServo; 
 
+Servo leftLeg;
+Servo leftFoot;
+Servo rightLeg;
+Servo rightFoot;
+int llCenter = 90;
+int lfCenter = 90;
+int rlCenter = 90;
+int rfCenter = 90;
+ // the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
-pinMode(LED_BUILTIN,OUTPUT);
-  myServo.attach(11);
-  myServo.write(90);
+leftLeg.attach(3,1000,2000);
+leftFoot.attach(5,1000,2000);
+rightLeg.attach(2,1000,2000);
+rightFoot.attach(4,1000,2000);
+
+rightFoot.write(rfCenter);
+leftLeg.write(llCenter);
+leftFoot.write(lfCenter);
+rightLeg.write(rlCenter);
+
+ //initialize digital pin LED_BUILTIN as an output
+pinMode(LED_BUILTIN, OUTPUT);
+myServo.attach(11,1000,2000);
+myServo.write(90);
+}
+void takeStep(){
+  leftLeg.write(llCenter);
+  leftFoot.write(lfCenter);
+  rightLeg.write(rlCenter);
+  rightFoot.write(rfCenter);
+
 }
 
-void blinkOnce(int time){
-  digitalWrite(LED_BUILTIN, HIGH);
-  myServo.write(140);
-  delay(time);
-  digitalWrite(LED_BUILTIN, LOW);
-  myServo.write(60);
-  delay(time);
-}
-
-void BlinkSOS() {
-  blinkOnce(1000);
-  blinkOnce(1000);
-  blinkOnce(1000);
-  blinkOnce(900);
-  blinkOnce(900);
-  blinkOnce(900);
-  blinkOnce(1000);
-  blinkOnce(1000);
-  blinkOnce(1000);
-}
 void loop(){
-  BlinkSOS();
+  takeStep();
 }
